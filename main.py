6079d9eb6c53e3cb61f737a58217dfeacb6b631c
unknown <Administrateur@L22.m2iformation.local>
2024-05-02 16:40:44 +0200

    add function cat_passwd() and chiffrer_passwd() to ensure the user account security

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from src.utils.generation import *
 
-print(generate_salt("abcd"))
+# print(generate_salt("abcd"))
 
 print(generate_key("abcd"))
+
+print(cat_passwd("abcd"))
+
+print(chiffrer_passwd("abcd"))
+
+# print(chiffrer_passwd("abcd"))
+
+# afficher_passwd_chiffred()
--- src/utils/generation.py
+++ src/utils/generation.py
@@ -14,6 +14,21 @@ def generate_salt(passwd):
 def generate_key(passwd):
     key=[]
     # while len(key)<=2*len(passwd):
-    randnumb=[random.randint(0, 10) for _ in range(2*len(passwd))]
+    randnumb=[random.randint(1, 10) for _ in range(2*len(passwd))]
     key+=randnumb
     return tuple(key)
+
+def cat_passwd(passwd):
+    return f"{passwd}{generate_salt(passwd)}"
+
+def chiffrer_passwd(passwd):
+    chiffred=[]
+    for i,elt in enumerate(cat_passwd(passwd)):
+        chiffred += ( ord(elt) + int(generate_key(passwd)[i]) ) % 128
+    return chiffred
+
+def afficher_passwd_chiffred():
+    for x in chiffrer_passwd():
+        print(f"{x} ")
+
+
